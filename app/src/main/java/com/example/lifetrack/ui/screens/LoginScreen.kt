b702370e970cf1b18cd279a017664c2c7629d8ba
dokwako <denzilwawire418@gmail.com>
2025-07-01 11:47:42 +0300

    initial commit

--- app/src/main/java/com/example/lifetrack/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/lifetrack/ui/screens/LoginScreen.kt
@@ -1,2 +1,10 @@
 package com.example.lifetrack.ui.screens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    Text("Login Screen Placeholder")
+}
